f3dc09b8a48edd1b0dffb0d6671c5e9a63e84143
web3saiful <web3saiful@gmail.com>
2025-12-24 20:31:53 +0600

    "update: ongoing learning & practice iterations"

--- Contracts/ERC721.sol
+++ Contracts/ERC721.sol
@@ -7,3 +7,27 @@ interface IERC165 {
         view
         returns (bool);
 } 
+
+interface IERC721 is IERC165 {
+    function balanceOf(address owner) external view returns (uint256 balance);
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+    function safeTransferFrom(address from, address to, uint256 tokenId)
+        external;
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) external;
+    function transferFrom(address from, address to, uint256 tokenId) external;
+    function approve(address to, uint256 tokenId) external;
+    function getApproved(uint256 tokenId)
+        external
+        view
+        returns (address operator);
+    function setApprovalForAll(address operator, bool _approved) external;
+    function isApprovedForAll(address owner, address operator)
+        external
+        view
+        returns (bool);
+}
